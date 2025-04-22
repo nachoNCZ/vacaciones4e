7a960619ecd9c036641fd3ae3f03ad24c77f0340
nachoNCZ <n.ias7890@gmail.com>
2025-04-22 17:09:33 -0600

    Proyecto inicial: entorno Django y script saludo.py

---
+++ saludo.py
@@ -0,0 +1,8 @@
+#Saludo
+
+def main():
+    nombre = input("Ingresa tu nombre: ")
+    print(f"Hola, {nombre} Bienvenido al sistema de gestión de vacaciones.")
+
+if __name__ == "__main__":
+    main()
